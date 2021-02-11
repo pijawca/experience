643ad4e12af732517a812b9969d1b8c3dc7681f8
pijawca <73064012+pijawca@users.noreply.github.com>
2021-02-11 20:51:42 +0800

    Add files via upload

---
+++ books/Python Programming for the Absolute Beginner, 3rd Edition/chapter 1/task_3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# -*- twitter.com/pijawca -*-
+"""
+Напишите программу, которая будет выводить на экран ваш любимый афоризм. Не забудьте о том, что автор
+афоризма должен быть упомянут на отдельной строке.
+"""
+
+author = 'Elon Musk - American entrepreneur, engineer and billionaire.'
+print(f'I don\'t ever give up.\n\t\t{author}')
